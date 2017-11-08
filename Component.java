0ac15fd42017ef4937a42f9a93aed9bc95077dac
am2209 <31702584+am2209@users.noreply.github.com>
2017-11-08 12:51:25 -0500

    Create Component.java

---
+++ Component.java
@@ -0,0 +1,20 @@
+public class Component
+{  
+   String name, type;
+   public Component(String n, ){
+      name = n;
+      type = "Component";
+   }
+   
+   public String getType(){
+      return type;
+   }
+   
+   public void setType(String t){
+      type = t;
+   }
+   
+   public int getNumComp(){
+      return numComponents;
+   } 
+}
